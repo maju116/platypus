9b040c57c13d65f352fbb5e5a24f52839dfca811
maju116 <michalmaj116@gmail.com>
2019-10-05 16:51:35 +0200

    First build

--- R/u_net.R
+++ R/u_net.R
@@ -35,45 +35,44 @@ u_net_down <- function(input, filters, kernel_size, batch_normalization = TRUE) 
 #' @return Downsalmling U-Net block
 #' @export
 u_net <- function(input_img, filters, dropout = 0.1, batch_normalization = TRUE) {
-  c1 <- u_net_down(input_img, filters * 1, kernel_size = 3, batchnorm = batchnorm)
+  c1 <- u_net_down(input_img, filters * 1, kernel_size = 3, batch_normalization = batch_normalization)
   p1 <- layer_max_pooling_2d(c1, pool_size = 2) %>%
     layer_dropout(rate = dropout)
 
-  c2 <- u_net_down(p1, filters * 2, kernel_size = 3, batchnorm = batchnorm)
+  c2 <- u_net_down(p1, filters * 2, kernel_size = 3, batch_normalization = batch_normalization)
   p2 <- layer_max_pooling_2d(c2, pool_size = 2) %>%
     layer_dropout(rate = dropout)
 
-  c3 <- u_net_down(p2, filters * 4, kernel_size = 3, batchnorm = batchnorm)
+  c3 <- u_net_down(p2, filters * 4, kernel_size = 3, batch_normalization = batch_normalization)
   p3 <- layer_max_pooling_2d(c3, pool_size = 2) %>%
     layer_dropout(rate = dropout)
 
-  c4 <- u_net_down(p3, filters * 8, kernel_size = 3, batchnorm = batchnorm)
+  c4 <- u_net_down(p3, filters * 8, kernel_size = 3, batch_normalization = batch_normalization)
   p4 <- layer_max_pooling_2d(c4, pool_size = 2) %>%
     layer_dropout(rate = dropout)
 
-  c5 <- u_net_down(p4, filters * 16, kernel_size = 3, batchnorm = batchnorm)
+  c5 <- u_net_down(p4, filters * 16, kernel_size = 3, batch_normalization = batch_normalization)
 
-  u6 <- layer_conv_2d_transpose(c5, filters * 8, kernel_size = 3, strides = 2) %>%
-    layer_concatenate(inputs = list(., c4)) %>%
+  u6 <- layer_conv_2d_transpose(c5, filters * 8, kernel_size = 3, strides = 2, padding = "same")
+  u6 <- layer_concatenate(inputs = list(u6, c4)) %>%
     layer_dropout(rate = dropout)
-  c6 <- u_net_down(u6, filters * 8, kernel_size = 3, batchnorm = batchnorm)
+  c6 <- u_net_down(u6, filters * 8, kernel_size = 3, batch_normalization = batch_normalization)
 
-  u7 <- layer_conv_2d_transpose(c6, filters * 4, kernel_size = 3, strides = 2) %>%
-    layer_concatenate(inputs = list(., c3)) %>%
+  u7 <- layer_conv_2d_transpose(c6, filters * 4, kernel_size = 3, strides = 2, padding = "same")
+  u7 <- layer_concatenate(inputs = list(u7, c3)) %>%
     layer_dropout(rate = dropout)
-  c7 <- u_net_down(u7, filters * 4, kernel_size = 3, batchnorm = batchnorm)
+  c7 <- u_net_down(u7, filters * 4, kernel_size = 3, batch_normalization = batch_normalization)
 
-  u8 <- layer_conv_2d_transpose(c7, filters * 2, kernel_size = 3, strides = 2) %>%
-    layer_concatenate(inputs = list(., c2)) %>%
+  u8 <- layer_conv_2d_transpose(c7, filters * 2, kernel_size = 3, strides = 2, padding = "same")
+  u8 <- layer_concatenate(inputs = list(u8, c2)) %>%
     layer_dropout(rate = dropout)
-  c8 <- u_net_down(u8, filters * 2, kernel_size = 3, batchnorm = batchnorm)
+  c8 <- u_net_down(u8, filters * 2, kernel_size = 3, batch_normalization = batch_normalization)
 
-  u9 <- layer_conv_2d_transpose(c8, filters * 1, kernel_size = 3, strides = 2) %>%
-    layer_concatenate(inputs = list(., c1)) %>%
+  u9 <- layer_conv_2d_transpose(c8, filters * 1, kernel_size = 3, strides = 2, padding = "same")
+  u9 <- layer_concatenate(inputs = list(u9, c1)) %>%
     layer_dropout(rate = dropout)
-  c9 <- u_net_down(u9, filters * 1, kernel_size = 3, batchnorm = batchnorm)
+  c9 <- u_net_down(u9, filters * 1, kernel_size = 3, batch_normalization = batch_normalization)
 
   output <- layer_conv_2d(c9, 1, 1, activation = "sigmoid")
   keras_model(inputs = input_img, outputs = output)
 }
-
