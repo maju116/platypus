1cf2fe400665d3d9e366c511bc20bcb5507b1348
maju116 <michalmaj116@gmail.com>
2019-10-08 14:33:34 +0200

    Add ability to read images from separate dirs. Merge multiple masks

--- R/generators.R
+++ R/generators.R
@@ -2,22 +2,26 @@
 #' @description Reads images from directory.
 #' @import keras
 #' @importFrom abind abind
-#' @importFrom purrr map
+#' @importFrom purrr map reduce
 #' @param paths Images directories.
 #' @param indices Indices of selected images. If `NULL` all images in `paths` will be selected.
 #' @param target_size Images size (height, width). Default to `c(256, 256)`.
 #' @param grayscale Boolean, whether to load the image as grayscale.
 #' @param scale Scaling factor for images pixel values. Default to `1 / 255`.
 #' @export
-read_images_from_directory <- function(paths, indices = NULL, target_size = c(256, 256), grayscale = FALSE, scale = 1 / 255) {
+read_images_from_directory <- function(paths, indices = NULL, target_size = c(256, 256),
+                                       grayscale = FALSE, scale = 1 / 255) {
   indices <- if (is.null(indices)) 1:length(paths) else indices
   map(paths[indices], ~ {
-    image_to_array(image_load(.x, grayscale = grayscale, target_size = target_size)) * scale
+    current_paths <- .x
+    current_paths %>% map(~ image_to_array(image_load(.x, grayscale = grayscale, target_size = target_size))) %>%
+      reduce(`+`) * scale
   }) %>% abind(along = 4) %>% aperm(c(4, 1, 2, 3))
 }
 
 #' Generates batches of data (images and masks). The data will be looped over (in batches).
 #' @description Generates batches of data (images and masks). The data will be looped over (in batches).
+#' @importFrom purrr map
 #' @param path Images and masks directory.
 #' @param only_images Should generator read only images (e.g. on train set for predictions).
 #' @param target_size Images / mask size (height, width). Default to `c(256, 256)`.
@@ -27,11 +31,17 @@ read_images_from_directory <- function(paths, indices = NULL, target_size = c(25
 #' @param shuffle Should data be shuffled.
 #' @param subdirs Vector of two characters containing names of subdirectories with images and masks.
 #' @export
-segmentation_generator <- function(path, only_images = FALSE, target_size = c(256, 256), grayscale = FALSE, scale = 1 / 255,
+segmentation_generator <- function(path, nested_paths = FALSE, only_images = FALSE, target_size = c(256, 256),
+                                   grayscale = FALSE, scale = 1 / 255,
                                    batch_size = 32, shuffle = TRUE, subdirs = c("images", "masks")) {
-  images_paths <- list.files(file.path(path, subdirs[1]), full.names  = TRUE)
-  if (!only_images) masks_paths <- list.files(file.path(path, subdirs[2]), full.names = TRUE)
-  # all(basename(images_paths) == basename(masks_paths))
+  if (!nested_paths) {
+    images_paths <- list.files(file.path(path, subdirs[1]), full.names  = TRUE) %>% as.list()
+    if (!only_images) masks_paths <- list.files(file.path(path, subdirs[2]), full.names = TRUE) %>% as.list()
+  } else {
+    images_dirs <- list.dirs(path, full.names  = TRUE, recursive = FALSE)
+    images_paths <- images_dirs %>% map(~ list.files(file.path(.x, subdirs[1]), full.names  = TRUE))
+    if (!only_images) masks_paths <- images_dirs %>% map(~ list.files(file.path(.x, subdirs[2]), full.names  = TRUE))
+  }
   print(paste0(length(images_paths), " images", if (!only_images) " with corresponding masks", " detected!"))
   i <- 1
   function() {
