b64cb8acbc8066085f47536a3eac18699f87a5f6
maju116 <michalmaj116@gmail.com>
2019-10-08 11:05:42 +0200

    Fix generator, add ability to read only images

--- R/generators.R
+++ R/generators.R
@@ -19,6 +19,7 @@ read_images_from_directory <- function(paths, indices = NULL, target_size = c(25
 #' Generates batches of data (images and masks). The data will be looped over (in batches).
 #' @description Generates batches of data (images and masks). The data will be looped over (in batches).
 #' @param path Images and masks directory.
+#' @param only_images Should generator read only images (e.g. on train set for predictions).
 #' @param target_size Images / mask size (height, width). Default to `c(256, 256)`.
 #' @param grayscale Boolean, whether to load the image as grayscale.
 #' @param scale Scaling factor for images pixel values. Default to `1 / 255`.
@@ -26,25 +27,24 @@ read_images_from_directory <- function(paths, indices = NULL, target_size = c(25
 #' @param shuffle Should data be shuffled.
 #' @param subdirs Vector of two characters containing names of subdirectories with images and masks.
 #' @export
-segmentation_generator <- function(path, target_size = c(256, 256), grayscale = FALSE, scale = 1 / 255,
+segmentation_generator <- function(path, only_images = FALSE, target_size = c(256, 256), grayscale = FALSE, scale = 1 / 255,
                                    batch_size = 32, shuffle = TRUE, subdirs = c("images", "masks")) {
   images_paths <- list.files(file.path(path, subdirs[1]), full.names  = TRUE)
-  masks_paths <- list.files(file.path(path, subdirs[2]), full.names = TRUE)
-  all(basename(images_paths) == basename(masks_paths))
+  if (!only_images) masks_paths <- list.files(file.path(path, subdirs[2]), full.names = TRUE)
+  # all(basename(images_paths) == basename(masks_paths))
   i <- 1
   function() {
     if (shuffle) {
       indices <- sample(1:length(images_paths), size = batch_size)
     } else {
-      if (i + batch_size >= length(images_paths))
-        i <<- 1
-      indices <- c(i:min(i + batch_size - 1, nrow(images_paths)))
-      i <<- i + length(indices)
+      indices <- c(i:min(i + batch_size - 1, length(images_paths)))
+      print(indices)
+      i <<- if (i + batch_size >= length(images_paths)) 1 else i + length(indices)
     }
     images <- read_images_from_directory(images_paths, indices = indices, target_size = target_size,
                                          grayscale = grayscale, scale = scale)
-    masks <- read_images_from_directory(masks_paths, indices = indices, target_size = target_size,
+    if (!only_images) masks <- read_images_from_directory(masks_paths, indices = indices, target_size = target_size,
                                         grayscale = TRUE, scale = 1 / 255) %>% to_categorical()
-    list(images, masks)
+    if (!only_images) list(images, masks) else list(images)
   }
 }
