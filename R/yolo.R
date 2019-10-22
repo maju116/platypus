7fd9d30bb0850fcb72cf39ce4fe461ec685e3dbf
maju116 <michalmaj116@gmail.com>
2019-10-22 23:45:22 +0200

    Fix skip connections

--- R/yolo.R
+++ R/yolo.R
@@ -1,5 +1,4 @@
-
-yolo3_conv2d <- function(input, skip_connection = FALSE, strides, filters, kernel_size,
+yolo3_conv2d <- function(input, skip_connection = NULL, strides, filters, kernel_size,
                          kernel_initializer = "he_normal", id,
                          batch_normalization = TRUE, leaky_relu = TRUE) {
   input %>%
@@ -11,11 +10,23 @@ yolo3_conv2d <- function(input, skip_connection = FALSE, strides, filters, kerne
                   kernel_initializer = kernel_initializer) %>%
     when(batch_normalization ~ layer_batch_normalization(., name = paste0("batch_norm_", id)), ~ .) %>%
     when(leaky_relu ~ layer_activation_leaky_relu(., alpha = 0.1, name = paste0("leaky_relu_", id)), ~ .) %>%
-    when(skip_connection ~ layer_add(list(input, .), name = paste0("leaky_relu_", id)), ~ .)
+    when(!is.null(skip_connection) ~ layer_add(list(skip_connection, .), name = paste0("add_", id)), ~ .)
 }
 
 yolo3 <- function(input_shape) {
-  input_img <- layer_input(shape = input_shape, name = 'input_img') %>%
-    yolo3_conv2d(skip_connection = FALSE, strides = 1, filters = 32,
-                 kernel_size = 3, id = 0,  leaky_relu = TRUE)
+  input_img <- layer_input(shape = input_shape, name = 'input_img')
+  block0 <- yolo3_conv2d(input_img, skip_connection = NULL, strides = 1, filters = 32,
+                 kernel_size = 3, id = 0, leaky_relu = TRUE,
+                 batch_normalization = TRUE)
+  block1 <- yolo3_conv2d(block0, skip_connection = NULL, strides = 2, filters = 64,
+                 kernel_size = 3, id = 1, leaky_relu = TRUE,
+                 batch_normalization = TRUE)
+  block2 <- yolo3_conv2d(block1, skip_connection = NULL, strides = 1, filters = 32,
+                 kernel_size = 1, id = 2, leaky_relu = TRUE,
+                 batch_normalization = TRUE)
+  block3 <- yolo3_conv2d(block2, skip_connection = block1, strides = 1, filters = 64,
+                 kernel_size = 3, id = 3, leaky_relu = TRUE,
+                 batch_normalization = TRUE)
+
+  keras_model(input_img, block3)
 }
