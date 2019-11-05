fe620f78aded33121573456366b75f22d43a0324
maju116 <michalmaj116@gmail.com>
2019-11-05 23:04:17 +0100

    Fix weights loading

--- R/load_darknet_weights.R
+++ R/load_darknet_weights.R
@@ -7,7 +7,7 @@ read_darknet_weights <- function(model, weights_path = "development/yolov3.weigh
     readBin(darknet_weights_file, integer(), n = 1, endian = "little", size = 4)
   }
   darknet_weights <- readBin(darknet_weights_file, double(), n = 62001757, endian = "little", size = 4)
-  layer_ids <- yolo3_config %>% map_dbl(~ .$id)
+  layer_ids <- yolo3_config %>% map_dbl(~ .$id) %>% c(80, 81, 92, 93) %>% sort()
   ind <- 1
   for (layer_id in layer_ids) {
     conv_layer <- get_layer(model, paste0("conv2d_", layer_id))
@@ -15,13 +15,13 @@ read_darknet_weights <- function(model, weights_path = "development/yolov3.weigh
     if (!(layer_id %in% c(81, 93, 105))) {
       batch_norm_layer <- get_layer(model, paste0("batch_norm_", layer_id))
       batch_norm_layer_shape <- dim(get_weights(batch_norm_layer)[[1]])
-      beta <- darknet_weights[ind:(batch_norm_layer_shape + ind - 1)]
+      beta <- darknet_weights[ind:(batch_norm_layer_shape + ind - 1)] %>% as.array()
       ind <- batch_norm_layer_shape + ind
-      gamma <- darknet_weights[ind:(batch_norm_layer_shape + ind - 1)]
+      gamma <- darknet_weights[ind:(batch_norm_layer_shape + ind - 1)] %>% as.array()
       ind <- batch_norm_layer_shape + ind
-      mean <- darknet_weights[ind:(batch_norm_layer_shape + ind - 1)]
+      mean <- darknet_weights[ind:(batch_norm_layer_shape + ind - 1)] %>% as.array()
       ind <- batch_norm_layer_shape + ind
-      var <- darknet_weights[ind:(batch_norm_layer_shape + ind - 1)]
+      var <- darknet_weights[ind:(batch_norm_layer_shape + ind - 1)] %>% as.array()
       ind <- batch_norm_layer_shape + ind
       set_weights(batch_norm_layer, list(gamma, beta, mean, var))
     }
@@ -33,7 +33,7 @@ read_darknet_weights <- function(model, weights_path = "development/yolov3.weigh
       kernel <- darknet_weights[ind:(prod(kernel_shape) + ind - 1)]
       kernel <- aperm(array_reshape(kernel, rev(kernel_shape)), c(3, 4, 2, 1))
       ind <- prod(kernel_shape) + ind
-      set_weights(conv_layer, list(kernel, bias))
+      set_weights(conv_layer, list(kernel, as.array(bias)))
     } else {
       kernel_shape <- dim(conv_layer_shape[[1]])
       kernel <- darknet_weights[ind:(prod(kernel_shape) + ind - 1)]
