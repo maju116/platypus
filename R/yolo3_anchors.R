89659bbf6454fa6c427df0e6a02a171d8dcdb7bf
Michał Maj <michalmaj116@gmail.com>
2020-06-30 19:32:45 +0200

    Finish k-median++ algorithm

--- R/yolo3_anchors.R
+++ R/yolo3_anchors.R
@@ -1,9 +1,21 @@
+#' Calculates Jaccard distance between two boxes.
+#' @description Calculates Jaccard distance between two boxes.
+#' @param box1_w Width of first box.
+#' @param box1_h Height of first box.
+#' @param box2_w Width of second box.
+#' @param box2_h Height of second box.
+#' @return Jaccard distance between two boxes.
 box_jaccard_distance <- function(box1_w, box1_h, box2_w, box2_h) {
   top <- min(box1_w, box2_w) * min(box1_h, box2_h)
   bottom <- box1_w * box1_h + box2_w * box2_h - top
   1 - top / bottom
 }
 
+#' Calculates initial anchor boxes for k-median++ algorithm.
+#' @description Calculates initial anchor boxes for k-median++ algorithm.
+#' @param annot_df `data.frame` with widths and heights of bounding boxes.
+#' @param total_anchors Number of anchors to generate.
+#' @return Initial anchor boxes for k-median++ algorithm.
 initialize_anchors <- function(annot_df, total_anchors) {
   initial_anchors <- annot_df %>% sample_n(1) %>% select(-label)
 
@@ -26,16 +38,28 @@ initialize_anchors <- function(annot_df, total_anchors) {
     mutate(anchor_id = 1:total_anchors)
 }
 
+#' Calculates anchor boxes using k-median++ algorithm.
+#' @description Calculates anchor boxes using k-median++ algorithm.
+#' @param anchors_per_grid Number of anchors per one grid.
+#' @param annot_paths List of annotations filepaths.
+#' @param labels Character vector containing class labels. For example \code{\link[platypus]{coco_labels}}.
+#' @param net_h Input layer height from trained \code{\link[platypus]{yolo3}} model. Must be divisible by `32`.
+#' @param net_w Input layer width from trained \code{\link[platypus]{yolo3}} model. Must be divisible by `32`.
+#' @param n_iter Maximum number of iteration for k-median++ algorithm.
+#' @param seed Random seed.
+#' @return List of anchor boxes.
+#' @export
 generate_anchors <- function(anchors_per_grid, annot_paths,
-                             labels, net_h, net_w, n_iter = 10) {
+                             labels, net_h, net_w, n_iter = 10, seed = 1234) {
+  set.seed(seed)
   total_anchors <- anchors_per_grid * 3
   annotations <- read_annotations_from_xml(annot_paths, NULL, "", labels)
   annot_df <- annotations %>% map_df(~ {
     image_h <- .x$height
     image_w <- .x$width
     .x$object %>%
-      mutate(box_w = round((xmax - xmin) / image_w * net_w),
-             box_h = round((ymax - ymin) / image_h * net_h)
+      mutate(box_w = (xmax - xmin) / image_w * net_w,
+             box_h = (ymax - ymin) / image_h * net_h
       )
   }) %>% select(box_w, box_h, label)
   print(annot_df %>% count(label))
@@ -61,7 +85,16 @@ generate_anchors <- function(anchors_per_grid, annot_paths,
     initial_anchors <- new_anchors
   }
   plot(base_plot + geom_point(data = new_anchors, color = "red", shape = 23))
-  new_anchors %>% mutate(area = box_w * box_h) %>%
+  new_anchors_arranged <- new_anchors %>%
+    mutate(box_w = round(box_w), box_h = round(box_h),
+           area = box_w * box_h) %>%
     arrange(desc(area)) %>% select(-area) %>%
-    mutate(anchor_id = 1:total_anchors)
+    select(-anchor_id)
+  1:3 %>% map(~ {
+    grid <- .x
+    new_anchors_arranged[((grid - 1) * anchors_per_grid + 1):(grid * anchors_per_grid), ] %>%
+      pmap(function(box_w, box_h) {
+        c(box_w, box_h)
+      })
+  })
 }
