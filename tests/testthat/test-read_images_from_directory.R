9d46bb33f15896c9cd9e388691878c4f069ae587
Michał Maj <michalmaj116@gmail.com>
2020-09-18 20:46:42 +0200

    Fix test for read_images_from_directory

--- tests/testthat/test-read_images_from_directory.R
+++ tests/testthat/test-read_images_from_directory.R
@@ -1,30 +1,77 @@
-# context("read_images_from_directory")
-#
-# test_that("read_images_from_directory reads images and masks correctly", {
-#   testdata_path <- system.file("testdata", package = "platypus")
-#   nested_dirs <- list.dirs(file.path(testdata_path, "nested_dirs"), full.names  = TRUE, recursive = FALSE)
-#   images_paths <- nested_dirs %>% purrr::map(~ list.files(file.path(.x, "images"), full.names  = TRUE))
-#   masks_paths <- nested_dirs %>% purrr::map(~ list.files(file.path(.x, "masks"), full.names  = TRUE))
-#   indices <- 2:3
-#   target_size <- c(2, 2)
-#   grayscale_images <- FALSE
-#   grayscale_masks <- TRUE
-#   scale <- 1 / 255
-#
-#   images <- read_images_from_directory(images_paths, indices, target_size, grayscale_images, scale)
-#   masks <- read_images_from_directory(masks_paths, indices, target_size, grayscale_masks, scale)
-#
-#   set.seed(666)
-#   sample_images <- array(sample(0:255, 3 * 2 * 2 * 3, replace = TRUE), dim = c(3, 2, 2, 3)) / 255
-#   sample_masks <- array(sample(c(0, 255), 6 * 2 * 2 * 3, replace = TRUE),
-#                         dim = c(6, 2, 2, 3))
-#   sample_masks <- 1:3 %>% purrr::map(~ {
-#     grayscale1 <- (0.2980392 * sample_masks[.x, , , 1] + 0.5843137 * sample_masks[.x, , , 2] + 0.1137255 * sample_masks[.x, , , 3])
-#     grayscale2 <- (0.2980392 * sample_masks[.x + 3, , , 1] + 0.5843137 * sample_masks[.x + 3, , , 2] + 0.1137255 * sample_masks[.x + 3, , , 3])
-#     grayscale <- reduce(list(grayscale1, grayscale2), `+`) / 255
-#     array_reshape(grayscale, c(2, 2, 1))
-#   }) %>% abind::abind(along = 4) %>% aperm(c(4, 1, 2, 3))
-#
-#   expect_true(all(abs(sample_images[indices, , , ] - images) < 0.005))
-#   expect_true(all(abs(sample_masks[indices, , , , drop = FALSE] - masks) < 0.005))
-# })
+context("read_images_from_directory")
+
+test_that("read_images_from_directory reads images and masks correctly from nested_dirs", {
+  testdata_path <- system.file("testdata", package = "platypus")
+  nested_dirs <- list.dirs(file.path(testdata_path, "nested_dirs"), full.names  = TRUE, recursive = FALSE)
+  images_paths <- nested_dirs %>% purrr::map(~ list.files(file.path(.x, "images"), full.names  = TRUE))
+  masks_paths <- nested_dirs %>% purrr::map(~ list.files(file.path(.x, "masks"), full.names  = TRUE))
+  indices <- NULL
+  target_size <- c(3, 3)
+  grayscale <- FALSE
+  scale <- 1 / 255
+  colormap <- list(c(0, 0, 0), c(111, 111, 111),
+                   c(222, 222, 222), c(255, 255, 255))
+
+  true_images <- read_images_from_directory(images_paths, indices, target_size, grayscale, scale, NULL)
+  true_masks <- read_images_from_directory(masks_paths, indices, target_size, grayscale, scale, colormap)
+
+  expected_images <- list(
+    array(c(255, 0, 0, 0, 111, 0, 111, 0, 222,
+            255, 0, 0, 0, 111, 0, 111, 0, 222,
+            255, 0, 0, 0, 111, 0, 111, 0, 222), dim = c(3, 3, 3)),
+    array(c(255, 0, 0, 0, 111, 111, 0, 0, 222,
+            255, 0, 0, 0, 111, 111, 0, 0, 222,
+            255, 0, 0, 0, 111, 111, 0, 0, 222), dim = c(3, 3, 3)),
+    array(c(222, 0, 0, 0, 111, 0, 111, 0, 255,
+            222, 0, 0, 0, 111, 0, 111, 0, 255,
+            222, 0, 0, 0, 111, 0, 111, 0, 255), dim = c(3, 3, 3))
+  ) %>% abind::abind(along = 4) %>% aperm(c(4, 1, 2, 3)) %>% `/`(255)
+
+  expected_masks <- list(
+    list(
+      array(c(0, 1, 1, 1, 0, 1, 0, 1, 0,
+              0, 1, 1, 1, 0, 1, 0, 1, 0,
+              0, 1, 1, 1, 0, 1, 0, 1, 0), dim = c(3, 3)),
+      array(c(0, 0, 0, 0, 1, 0, 1, 0, 0,
+              0, 0, 0, 0, 1, 0, 1, 0, 0,
+              0, 0, 0, 0, 1, 0, 1, 0, 0), dim = c(3, 3)),
+      array(c(0, 0, 0, 0, 0, 0, 0, 0, 1,
+              0, 0, 0, 0, 0, 0, 0, 0, 1,
+              0, 0, 0, 0, 0, 0, 0, 0, 1), dim = c(3, 3)),
+      array(c(1, 0, 0, 0, 0, 0, 0, 0, 0,
+              1, 0, 0, 0, 0, 0, 0, 0, 0,
+              1, 0, 0, 0, 0, 0, 0, 0, 0), dim = c(3, 3))
+    ) %>% abind::abind(along = 3),
+    list(
+      array(c(0, 1, 1, 1, 0, 0, 1, 1, 0,
+              0, 1, 1, 1, 0, 0, 1, 1, 0,
+              0, 1, 1, 1, 0, 0, 1, 1, 0), dim = c(3, 3)),
+      array(c(0, 0, 0, 0, 1, 1, 0, 0, 0,
+              0, 0, 0, 0, 1, 1, 0, 0, 0,
+              0, 0, 0, 0, 1, 1, 0, 0, 0), dim = c(3, 3)),
+      array(c(0, 0, 0, 0, 0, 0, 0, 0, 1,
+              0, 0, 0, 0, 0, 0, 0, 0, 1,
+              0, 0, 0, 0, 0, 0, 0, 0, 1), dim = c(3, 3)),
+      array(c(1, 0, 0, 0, 0, 0, 0, 0, 0,
+              1, 0, 0, 0, 0, 0, 0, 0, 0,
+              1, 0, 0, 0, 0, 0, 0, 0, 0), dim = c(3, 3))
+    ) %>% abind::abind(along = 3),
+    list(
+      array(c(0, 1, 1, 1, 0, 1, 0, 1, 0,
+              0, 1, 1, 1, 0, 1, 0, 1, 0,
+              0, 1, 1, 1, 0, 1, 0, 1, 0), dim = c(3, 3)),
+      array(c(0, 0, 0, 0, 1, 0, 1, 0, 0,
+              0, 0, 0, 0, 1, 0, 1, 0, 0,
+              0, 0, 0, 0, 1, 0, 1, 0, 0), dim = c(3, 3)),
+      array(c(1, 0, 0, 0, 0, 0, 0, 0, 0,
+              1, 0, 0, 0, 0, 0, 0, 0, 0,
+              1, 0, 0, 0, 0, 0, 0, 0, 0), dim = c(3, 3)),
+      array(c(0, 0, 0, 0, 0, 0, 0, 0, 1,
+              0, 0, 0, 0, 0, 0, 0, 0, 1,
+              0, 0, 0, 0, 0, 0, 0, 0, 1), dim = c(3, 3))
+    ) %>% abind::abind(along = 3)
+  ) %>% abind::abind(along = 4) %>% aperm(c(4, 1, 2, 3))
+
+  expect_equal(expected_images, true_images)
+  expect_equal(expected_masks, true_masks)
+})
