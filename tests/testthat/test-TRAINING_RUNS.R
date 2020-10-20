08618856c9c39f0eb0554961f8f1587d503c8880
Michał Maj <michalmaj116@gmail.com>
2020-10-20 20:18:49 +0200

    Add yolo3 TRAINING RUN test

--- tests/testthat/test-TRAINING_RUNS.R
+++ tests/testthat/test-TRAINING_RUNS.R
@@ -29,7 +29,46 @@ test_that("u_net model can be fitted with no errors", {
   sample_generator <- segmentation_generator(path, colormap, mode, only_images, net_h, net_w,
                                              grayscale, scale, batch_size, shuffle, subdirs)
   start_weights <- get_weights(model)
-  model %>% fit_generator(sample_generator, steps_per_epoch = 3, epochs = 2)
+  model %>% fit_generator(sample_generator, steps_per_epoch = 1, epochs = 2)
+  end_weights <- get_weights(model)
+  expect_true(!identical(start_weights, end_weights))
+})
+
+test_that("yolo3 model can be fitted with no errors", {
+  testdata_path <- system.file("testdata", package = "platypus")
+  images_path <- file.path(testdata_path, "images/")
+  annot_path <- file.path(testdata_path, "annotations/")
+  labels <- coco_labels
+  n_class <- length(labels)
+  net_h <- 96
+  net_w <- 96
+  grayscale <- FALSE
+  anchors_per_grid <- 3
+  anchors <- coco_anchors
+  model <- yolo3(
+    net_h = net_h,
+    net_w = net_w,
+    grayscale = grayscale,
+    n_class = n_class,
+    anchors = anchors
+  )
+  model %>% compile(
+    optimizer = optimizer_adam(lr = 1e-5),
+    loss = yolo3_loss(anchors, n_class = n_class),
+    metrics = yolo3_metrics(anchors, n_class = n_class)
+  )
+  sample_generator <- yolo3_generator(
+    annot_path = annot_path,
+    images_path = images_path,
+    net_h = net_h,
+    net_w = net_w,
+    batch_size = 2,
+    shuffle = FALSE,
+    labels = labels,
+    annot_format = "pascal_voc"
+  )
+  start_weights <- get_weights(model)
+  model %>% yolo3_fit_generator(sample_generator, steps_per_epoch = 1, epochs = 2)
   end_weights <- get_weights(model)
   expect_true(!identical(start_weights, end_weights))
 })
